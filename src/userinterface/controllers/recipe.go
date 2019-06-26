9669b0926b7b8a77606bcc4d6956f0e900e155ad
wuxutao <wuxutao@mafengwo.cn>
2019-06-26 23:53:37 +0800

    add go runtime

--- src/userinterface/controllers/recipe.go
+++ src/userinterface/controllers/recipe.go
@@ -3,13 +3,17 @@ package controllers
 import (
 	"github.com/kataras/iris/mvc"
 	"github.com/kataras/iris"
+	"sync"
+	"fmt"
 
 	"../../domain/service"
+	"../../infrastructure/dao"
 )
 
 type RecipeController struct {}
 
 var materialService service.MaterialService = service.MaterialService{}
+var recipeService service.RecipeService = service.RecipeService{}
 
 func (c *RecipeController) Get() mvc.Result {
 
@@ -23,12 +27,40 @@ func (c *RecipeController) Get() mvc.Result {
 }
 
 func (c *RecipeController) GetBy(foodId int64) mvc.Result {
-	materialList := materialService.GetList(1, 1000)
+	
+
+	materialList := make(chan []dao.Material)
+	recipeList := make(chan []dao.Recipe)
+
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	go func() {
+		defer wg.Done()
+		fmt.Println("fbbb")
+		materialList <- materialService.GetList(1, 1000)
+	}()
+
+	go func() {
+		defer wg.Done()
+		recipeList <- recipeService.GetList(foodId, 1, 1000)
+	}()
+
+	go func() {
+		fmt.Println("wait")
+		wg.Wait()
+		close(materialList)
+		close(recipeList)
+	}()
+	fmt.Println("end")
+	
+
 	return mvc.View{
 		Name: "recipe/edit.html",
 		Data: map[string]interface{} {
 			"Title": "Index",
 			"materialList": materialList,
+			"recipeList": recipeList,
 			"foodId": foodId,
 		},
 	}
